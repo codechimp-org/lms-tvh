c558703723c0a6adc2d344f7e742fba9b91958fe
Andrew Jackson <andrew@codechimp.org>
2019-05-28 19:58:32 +0100

    Fix preferences

--- plugin/Settings.pm
+++ plugin/Settings.pm
@@ -27,33 +27,33 @@ sub beforeRender {
 	my @prefstationsortingOpts  =({ stationsorting  =>   'NAME',  stationsortingtext  => string('PLUGIN_TVH_STATION_SORTING_NAME')},				
 								{ stationsorting  =>   'NUMBER',  stationsortingtext =>  string('PLUGIN_TVH_STATION_SORTING_NUMBER')});
 
-	$params->{'stationsortingopts'}  = \@prefstationsortingOpts;
+	$params->{'pref_stationsortingopts'}  = \@prefstationsortingOpts;
 }
 
 sub handler {
 	my ($class, $client, $params) = @_;
 	if ($params->{'saveSettings'}) 	{				# SAVE MODE
-		my $server = $params->{'server'};
+		my $server = $params->{'pref_server'};
 		$prefs->set('server', "$server");
-		my $port = $params->{'port'};
+		my $port = $params->{'pref_port'};
 		$prefs->set('port', "$port");
-		my $username = $params->{'username'};
+		my $username = $params->{'pref_username'};
 		$prefs->set('username', "$username");		
-		my $password = $params->{'password'};
+		my $password = $params->{'pref_password'};
 		$prefs->set('password', "$password");
-		my $profile = $params->{'profile'};
+		my $profile = $params->{'pref_profile'};
 		$prefs->set('profile', "$profile");
-		my $stationsorting = $params->{'stationsorting'};
+		my $stationsorting = $params->{'pref_stationsorting'};
 		$prefs->set('stationsorting', "$stationsorting");
 	}				
 	
 	# LOAD
-	$params->{'prefs'}->{'server'} = $prefs->get('server');
-	$params->{'prefs'}->{'port'} = $prefs->get('port');
-	$params->{'prefs'}->{'username'} = $prefs->get('username');
-	$params->{'prefs'}->{'password'} = $prefs->get('password');
-	$params->{'prefs'}->{'profile'} = $prefs->get('profile');
-	$params->{'prefs'}->{'stationsorting'} = $prefs->get('stationsorting');
+	$params->{'prefs'}->{'pref_server'} = $prefs->get('server');
+	$params->{'prefs'}->{'pref_port'} = $prefs->get('port');
+	$params->{'prefs'}->{'pref_username'} = $prefs->get('username');
+	$params->{'prefs'}->{'pref_password'} = $prefs->get('password');
+	$params->{'prefs'}->{'pref_profile'} = $prefs->get('profile');
+	$params->{'prefs'}->{'pref_stationsorting'} = $prefs->get('stationsorting');
 	
 	return $class->SUPER::handler($client, $params);
 }
