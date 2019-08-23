e8d8ce9808186e4d12f71649b71128cceb612c1d
Andrew Jackson <andrew@codechimp.org>
2019-08-23 10:07:52 +0100

    Check for debug log available

--- plugin/API.pm
+++ plugin/API.pm
@@ -78,7 +78,7 @@ sub _call {
 	# 	return;
 	# }
 	# elsif ( main::INFOLOG && $log->is_info ) {
-	 	$log->debug("API call: $url");
+	 	main::DEBUGLOG && $log->is_debug && $log->debug("API call: $url");
 	# }
 
 	Slim::Networking::SimpleAsyncHTTP->new(
